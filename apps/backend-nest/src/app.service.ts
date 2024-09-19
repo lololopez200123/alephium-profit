df5448a7437d208a9807e106aba36e38a4a2c0fd
Lorenzo Lopez <lorenzolopez200123@gmail.com>
2024-09-19 19:49:19 -0300

    refactor: :coffin: change name app service app

--- apps/backend-nest/src/app.service.ts
+++ apps/backend-nest/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Santander API Candidates!';
+    return 'Alephium Profit API!';
   }
 }

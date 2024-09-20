e3635500d2ff871dc73d2fa25f043c12b0776a06
matias_ibanez <matias_ibanez@hotmail.com>
2024-09-20 09:56:32 -0300

    feat: :construction: Profit structure
    Complete structure for profit, add graphics and coins

--- apps/frontend-next/app/profit/page.tsx
+++ apps/frontend-next/app/profit/page.tsx
@@ -1,122 +1,241 @@
 import Chart from "@/components/Chart";
 import { Box, Typography } from "@mui/material";
 import Image from "next/image";
-import React from "react";
 
 function ProfitChards() {
   const coin = [
     {
       name: "ALEPHIUM",
-      amount: "10 ALPH",
-      price: "3.10",
+      amount: "5000",
+      priceInAlph: "1500",
       cant: 1.223453,
-      pnl: "-2.78",
+      pnl: 0.8,
     },
-    { name: "USDT", amount: "10 ALPH ", price: "1" },
-    { name: "WBTC", amount: "2100 ALPH", price: "60000" },
-    { name: "WETH", amount: "12 ALPH", price: "2122" },
-    { name: "ALEPHIUM", amount: "10 ALPH", price: "2.5" },
+    {
+      name: "USDT",
+      amount: "15000",
+      priceInAlph: "1500",
+      cant: 1.223453,
+      pnl: -2.78,
+    },
+    {
+      name: "WBTC",
+      amount: "5000",
+      priceInAlph: "1500",
+      cant: 1.223453,
+      pnl: 2.78,
+    },
+    {
+      name: "WETH",
+      amount: "5000",
+      priceInAlph: "1500",
+      cant: 1.223453,
+      pnl: -2.78,
+    },
+    {
+      name: "ALEPHIUM",
+      amount: "5000",
+      priceInAlph: "1500",
+      cant: 1.223453,
+      pnl: 0.8,
+    },
+    {
+      name: "USDT",
+      amount: "15000",
+      priceInAlph: "1500",
+      cant: 1.223453,
+      pnl: -2.78,
+    },
+    {
+      name: "WBTC",
+      amount: "5000",
+      priceInAlph: "1500",
+      cant: 1.223453,
+      pnl: 2.78,
+    },
+    {
+      name: "WETH",
+      amount: "5000",
+      priceInAlph: "1500",
+      cant: 1.223453,
+      pnl: -2.78,
+    },
   ];
+  const selectedTime = ["1D", "1W", "1M", "1Y"];
 
+  //   const [selectCoin, setSelectCoin] = useState(null);
+
+  //   const handleSelectCoin = (coin) => {
+  //     setSelectCoin(coin); // Actualiza el estado con la moneda seleccionada
+  //   };
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        paddingBlock: "78px",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+        paddingX: "1rem",
+        overflow: "hidden",
+      }}
+    >
       <Box
         sx={{
           width: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          position: "fixed",
-          top: 60,
         }}
       >
         <Typography variant="caption">PROFIT CHARDS</Typography>
       </Box>
+      {/*Graph*/}
+      <Box sx={{ paddingTop: "2rem" }}>
+        <Typography sx={{ fontSize: "0.9375rem" }}>{coin[0].name}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "baseline",
+          }}
+        >
+          <Typography sx={{ fontSize: "2.5rem" }}>{coin[0].cant}</Typography>
+          <Typography
+            sx={{
+              fontSize: "0.9375rem",
+              paddingLeft: "1%",
+              color: true ? "rgba(226, 66, 66, 1)" : "rgba(40, 231, 197, 1)",
+            }}
+          >
+            {coin[0].pnl}
+          </Typography>
+        </Box>
+        <Chart></Chart>
+      </Box>
+      {/*Selector*/}
       <Box
         sx={{
+          width: "80%",
+          marginInline: "auto",
+          height: "2rem",
           display: "flex",
-          marginTop: "30%",
-          paddingBottom: "78px",
-          flexDirection: "column",
-          width: "100%",
-          height: "100%",
-          paddingX: "1rem",
+          justifyContent: "space-around",
+
+          marginBottom: "1rem",
+        }}
+      >
+        {selectedTime?.map((id) => (
+          <Box
+            key={id} // key debe estar como prop del Box
+            sx={{
+              width: "42px",
+              height: "29px",
+              borderRadius: "5px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              background: false
+                ? "linear-gradient(180deg, #28E7C5 -100%, #0B1426 170.69%)"
+                : "linear-gradient(180deg, rgba(255, 255, 255, 0.15) -142.19%, rgba(11, 20, 38, 0.15) 214.06%)",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: ".9rem",
+                textAlign: "center",
+              }}
+            >
+              {id}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+      {/*Favorite Coins */}
+      <Typography
+        sx={{ marginBottom: "5px", marginLeft: "20px" }}
+        fontSize="0.625rem"
+        variant="caption"
+      >
+        MY GRAPHS
+      </Typography>
+      <Box
+        sx={{
           overflow: "auto",
+          height: "50%",
         }}
       >
         <Box>
-          <Typography sx={{ fontSize: "0.9375rem" }}>{coin[0].name}</Typography>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "flex-start",
-              alignItems: "baseline",
-            }}
-          >
-            <Typography sx={{ fontSize: "2.5rem" }}>{coin[0].cant}</Typography>
-            <Typography
+          {coin?.map((item, index) => (
+            <Box
+              key={index}
               sx={{
-                fontSize: "0.9375rem",
-                paddingLeft: "1%",
-                color: true ? "rgba(226, 66, 66, 1)" : "rgba(40, 231, 197, 1)",
+                display: "flex",
+                justifyContent: "space-between",
+                borderRadius: "10px",
+                height: "4rem",
+                marginBlock: ".5rem",
+                background: `linear-gradient(180deg, rgba(255, 255, 255, 0.15) -142.19%, rgba(11, 20, 38, 0.15) 214.06%)`,
               }}
             >
-              {coin[0].pnl}
-            </Typography>
-          </Box>
-          <Chart></Chart>
-        </Box>
-        <Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              borderRadius: "10px",
-              background: `linear-gradient(180deg, rgba(255, 255, 255, 0.15) -142.19%, rgba(11, 20, 38, 0.15) 214.06%)`,
-            }}
-          >
-            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "24px",
+                    height: "24px",
+                    background: "white",
+                    borderRadius: "30px",
+                    margin: "0.8rem",
+                    marginInline: "1.25rem",
+                  }}
+                >
+                  <Image
+                    width="16"
+                    height="16"
+                    alt={item.name}
+                    src="/ALPHAGA.png"
+                  />
+                </Box>
+                <Typography variant="subtitle2">{item.name}</Typography>
+                <Typography
+                  variant="caption"
+                  sx={{
+                    paddingLeft: ".9rem",
+                    fontSize: "0.625rem",
+                    color:
+                      item?.pnl <= 0
+                        ? "rgba(226, 66, 66, 1)"
+                        : "rgba(40, 231, 197, 1)",
+                  }}
+                >
+                  {item.pnl}
+                </Typography>
+              </Box>
+
+              {/* Sección de datos */}
               <Box
                 sx={{
+                  width: "25%",
                   display: "flex",
-                  alignItems: "center",
+                  alignItems: "left",
+                  flexDirection: "column",
+                  marginInline: "3%",
                   justifyContent: "center",
-                  width: "24px",
-                  height: "24px",
-                  background: "white",
-                  borderRadius: "30px",
-                  margin: "0.8rem",
-                  marginInline: "1.25rem",
                 }}
               >
-                <Image
-                  width="16"
-                  height="16"
-                  alt="1"
-                  src="/ALPHAGA.png"
-                ></Image>
+                <Typography variant="subtitle1">{item.amount}</Typography>
+                <Typography fontSize="0.625rem" variant="caption">
+                  {item.priceInAlph} ALPH
+                </Typography>
               </Box>
-              <Typography variant="subtitle2">moneda</Typography>
             </Box>
-            <Box
-              sx={{
-                width: "16%",
-                display: "flex",
-                alignItems: "left",
-                flexDirection: "column",
-                marginInline: "3%",
-                justifyContent: "center",
-              }}
-            >
-              <Typography variant="subtitle1">{1234}</Typography>
-              <Typography fontSize="0.625rem" variant="caption">
-                {23}
-              </Typography>
-            </Box>
-          </Box>
+          ))}
         </Box>
       </Box>
-    </>
+    </Box>
   );
 }
 

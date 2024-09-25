4d4d7c4bc0da9699002ba7e3d5ce5f5c1501d72a
matias_ibanez <matias_ibanez@hotmail.com>
2024-09-25 18:29:18 -0300

    feat: :sparkles: Change in navTop and ButtonConnectWallet
    change all page titles to uppercase, and add the name in the button wallet

--- apps/frontend-next/components/ButtonConnectWallet.tsx
+++ apps/frontend-next/components/ButtonConnectWallet.tsx
@@ -1,12 +1,16 @@
-import { AlephiumConnectButton } from '@alephium/web3-react';
-import { useState } from 'react';
-import { Button, Box, Typography, Modal, IconButton } from '@mui/material';
-import Image from 'next/image';
-import nookies from 'nookies';
+import { AlephiumConnectButton } from "@alephium/web3-react";
+import { useState } from "react";
+import { Button, Box, Typography, Modal, IconButton } from "@mui/material";
+import Image from "next/image";
+import nookies from "nookies";
+
+import { useAtom } from "jotai";
+import { userAtom } from "@/store/userAtom";
 
 const ButtonConnectWallet = () => {
   const [open, setOpen] = useState(false);
 
+  const [user, setUser] = useAtom(userAtom);
   const handleOpen = () => {
     setOpen(true);
   };
@@ -17,13 +21,19 @@ const ButtonConnectWallet = () => {
   return (
     <AlephiumConnectButton.Custom>
       {({ isConnected, disconnect, show, account }) => {
-        const firstPart = account?.address.substring(0, 6);
-        const lastPart = account?.address.substring(account?.address.length - 6);
-        const modifiedText = firstPart + '...' + lastPart;
-
+        let text: string = "";
+        if (user?.name) {
+          text = user.name;
+        } else {
+          const firstPart = account?.address?.substring(0, 6);
+          const lastPart = account?.address?.substring(
+            account?.address?.length - 6
+          );
+          text = firstPart + "..." + lastPart;
+        }
         const handleDisconnect = () => {
           localStorage.clear();
-          nookies.destroy(null, 'jwt');
+          nookies.destroy(null, "jwt");
           disconnect();
         };
 
@@ -32,38 +42,47 @@ const ButtonConnectWallet = () => {
             <Button
               onClick={handleOpen}
               sx={{
-                width: '150px',
-                color: 'white',
-                textTransform: 'none',
-                paddingInline: '1rem',
-                height: '30px',
-                background: 'linear-gradient(90deg, #6942E2 0%,  #28E7C5 99%)',
-                borderRadius: '41px',
-                fontSize: { xs: '12px', sm: '14px' },
+                width: "150px",
+                color: "white",
+                textTransform: "none",
+                paddingInline: "1rem",
+                height: "30px",
+                background: "linear-gradient(90deg, #6942E2 0%,  #28E7C5 99%)",
+                borderRadius: "41px",
+                fontSize: { xs: "12px", sm: "14px" },
               }}
             >
-              {modifiedText}
+              {text}
             </Button>
-            <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
               <Box
                 sx={{
-                  position: 'absolute',
-                  top: '50%',
-                  left: '50%',
-                  transform: 'translate(-50%, -50%)',
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
                   width: 400,
-                  bgcolor: 'black',
-                  border: '2px solid #000',
-                  borderRadius: '20px',
+                  bgcolor: "black",
+                  border: "2px solid #000",
+                  borderRadius: "20px",
                   boxShadow: 24,
                   p: 4,
-                  textAlign: 'center',
+                  textAlign: "center",
                 }}
               >
-                <IconButton sx={{ position: 'absolute', right: 0, top: 0 }} edge="start" onClick={handleClose}>
+                <IconButton
+                  sx={{ position: "absolute", right: 0, top: 0 }}
+                  edge="start"
+                  onClick={handleClose}
+                >
                   <Image alt="close" src="/close.svg" width="25" height="25" />
                 </IconButton>
-                <Typography variant="h6">{modifiedText}</Typography>
+                <Typography variant="h6">{text}</Typography>
                 <Button onClick={handleDisconnect}>Disconnect</Button>
               </Box>
             </Modal>
@@ -72,14 +91,14 @@ const ButtonConnectWallet = () => {
           <Button
             onClick={show}
             sx={{
-              width: '150px',
-              color: 'white',
-              textTransform: 'none',
-              paddingInline: '1rem',
-              height: '30px',
-              background: 'linear-gradient(90deg, #6942E2 0%,  #28E7C5 99%)',
-              borderRadius: '41px',
-              fontSize: { xs: '12px', sm: '14px' },
+              width: "150px",
+              color: "white",
+              textTransform: "none",
+              paddingInline: "1rem",
+              height: "30px",
+              background: "linear-gradient(90deg, #6942E2 0%,  #28E7C5 99%)",
+              borderRadius: "41px",
+              fontSize: { xs: "12px", sm: "14px" },
             }}
           >
             Connect

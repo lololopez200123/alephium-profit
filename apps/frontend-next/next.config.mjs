e77ffdd7d4aa6949d3d564c32fb315c66cdf3db9
Eduardo210 <victorevivanco@gmail.com>
2024-09-24 21:28:16 -0300

    style: :art: next config github images

--- apps/frontend-next/next.config.mjs
+++ apps/frontend-next/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['raw.githubusercontent.com'], // Add allowed image domains here
+  },
+};
 
 export default nextConfig;

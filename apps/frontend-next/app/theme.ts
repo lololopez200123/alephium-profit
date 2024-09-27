a1862909b87977a1106a078278b9328fcc1e43bf
Lorenzo Lopez <lorenzolopez200123@gmail.com>
2024-09-27 02:26:47 -0300

    feat: :sparkles: added config theme material ui

--- apps/frontend-next/app/theme.ts
+++ apps/frontend-next/app/theme.ts
@@ -1,11 +1,26 @@
-// theme MUI
-"use client";
-
-import { createTheme } from "@mui/material/styles";
+'use client';
+import { createTheme } from '@mui/material/styles';
 
 const themeConfig = createTheme({
   typography: {
-    fontFamily: "Poppins, sans-serif",
+    fontFamily: 'Poppins, sans-serif',
+  },
+  palette: {
+    primary: {
+      main: '#28E7C5',
+    },
+    secondary: {
+      main: '#6942E2',
+    },
+    text: {
+      primary: '#FEFEFC',
+    },
+    error: {
+      main: '#ff0000',
+    },
+    background: {
+      default: '#0B1426',
+    },
   },
 });
 

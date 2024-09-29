a7a96e9715bca4d964a470318e52d176c43a388f
Lorenzo Lopez <lorenzolopez200123@gmail.com>
2024-09-28 21:06:20 -0300

    feat: :sparkles: add metadata for SEO

--- apps/frontend-next/app/layout.tsx
+++ apps/frontend-next/app/layout.tsx
@@ -11,7 +11,54 @@ import Loading from '@/components/loading/Loading';
 
 export const metadata: Metadata = {
   title: 'Alephium Profit',
-  description: 'Alephium Profit dApp',
+  description: 'Alephium Profit dApp Follow up your profits in Alephium.',
+  keywords: ['Alephium', 'Alph', 'web3', 'PNL', 'Profit', 'dApp', 'Blockchain', 'Investments'],
+  authors: [{ name: 'Alephium Profit', url: 'https://alephium-profit.com' }],
+  metadataBase: new URL('https://alephium-profit.com'),
+  alternates: {
+    canonical: 'https://alephium-profit.com',
+  },
+  openGraph: {
+    title: 'Alephium Profit',
+    description: 'Alephium Profit dApp Follow up your profits in Alephium.',
+    url: 'https://alephium-profit.com',
+    siteName: 'Alephium Profit',
+    locale: 'es_ES',
+    type: 'website',
+    images: [
+      {
+        url: '/open-graph.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Alephium Profit - Follow up your profits in Alephium',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Alephium Profit',
+    description: 'Alephium Profit DAPP to follow up on your profits on the Alephium network.',
+    images: ['/open-graph.jpg'],
+    creator: '@AlephiumProFitBot', // Opcional
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/profit-logo.svg',
+    apple: '/profit-logo.svg',
+  },
 };
 
 export default function RootLayout({

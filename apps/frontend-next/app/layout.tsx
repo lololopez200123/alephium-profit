536c28941ff8e41b79f6f682e6b6d569bcad6f35
Lorenzo Lopez <lorenzolopez200123@gmail.com>
2024-09-30 18:30:05 -0300

    feat: :bug: added maximum scale for IOS mobile chrome

--- apps/frontend-next/app/layout.tsx
+++ apps/frontend-next/app/layout.tsx
@@ -62,6 +62,13 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
